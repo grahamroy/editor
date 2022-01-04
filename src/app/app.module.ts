128b9d307407fbc11fdd0548c138e1c238afc97a
Graham <grahamroy@hotmail.com>
2022-01-04 14:29:43 +0000

    added FormsModule to make it work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,8 @@ import { NgxEditorModule } from 'ngx-editor';
   ],
   imports: [
     BrowserModule,
-    NgxEditorModule
+    NgxEditorModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
